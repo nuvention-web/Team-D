662426c55b82fb9f28a62c32889cb5bbc7d49133
Prithvi S S <prithvisrinivasasan-swachandam2017@u.northwestern.edu>
2017-02-27 19:43:07 -0600

    Raw_landing.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './app.jsx';
+import Landing from './landing.jsx'
 
-render( <AppContainer><App/></AppContainer>, document.querySelector("#app"));
+render( <AppContainer><Landing/></AppContainer>, document.querySelector("#app"));
 
 if (module && module.hot) {
   module.hot.accept('./app.jsx', () => {
     const App = require('./app.jsx').default;
     render(
       <AppContainer>
-        <App/>
+        <Landing/>
       </AppContainer>,
       document.querySelector("#app")
     );
