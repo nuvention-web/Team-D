0aef2a90ca4040f54502c04f00d5627dee8639e7
lennyKim <heliotherapyy@gmail.com>
2017-04-10 19:57:56 -0500

    (fix) change filenames

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './App.jsx';
-import Landing from './Landing.jsx'
+import App from './app.jsx';
+import Landing from './landing.jsx'
 
 render( <AppContainer><Landing/></AppContainer>, document.querySelector("#app"));
 
 if (module && module.hot) {
-  module.hot.accept('./App.jsx', () => {
-    const App = require('./App.jsx').default;
+  module.hot.accept('./app.jsx', () => {
+    const App = require('./app.jsx').default;
     render(
       <AppContainer>
         <Landing/>
