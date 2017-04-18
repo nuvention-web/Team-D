6fb951965b95ffd296949af0a2efa2a304a4cbe0
Megan Conlon <meganconlon2018@u.northwestern.edu>
2017-04-17 22:16:56 -0500

    started layout

--- src/components/Platform.js
+++ src/components/Platform.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 export const Platform = (props) => {
   return (
-    <form onSubmit = {props.handleSubmit} id = "platforms">
+    <div id = "views">
+      <h1>VIEWS</h1>
+      <form onSubmit = {props.handleSubmit} id = "platforms">
       <div id= "fb_checkbox">
           <input type="checkbox"  value = "Facebook" onChange = {props.handleFBChange} />
           <label>Facebook</label>
       </div>
       <div id = "ig_checkbox">
           <input type="checkbox" value = "Instagram" onChange = {props.handleInstaChange} />
-          <label>Instagram (not functional)</label>
+          <label>Youtube</label>
       </div>
     </form>
+    </div>
   )
 }
