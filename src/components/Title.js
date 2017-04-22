a32f4d008798385efa60695ff5456ff50c65b765
Megan Conlon <meganconlon2018@u.northwestern.edu>
2017-04-22 16:27:41 -0500

    test css changes

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ export const Title = (props) => {
     <section className="topTitle">
       <div id = "top">
       <img src={require("../../images/logo.png")} id = "logo_img"/>
-      <h1 id = "title">TELEOS</h1>
+      <h1 id = "title"> > ALL PLATFORMS</h1>
       <hr id = "top_divider"></hr>
       </div>
     </section>
