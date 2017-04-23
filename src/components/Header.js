df210c685521a5cc75a1675e43b5e4006937496c
Megan Conlon <meganconlon2018@u.northwestern.edu>
2017-04-22 20:33:00 -0500

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,13 @@ export class Header extends React.Component {
 
   render() {
     return (
-      <p>This is Header</p>
+     	<section className="topTitle">
+      	<div id = "top">
+      		<img src={require("../../images/logo.png")} id = "logo_img"/>
+      		<h1 id = "title"> > ALL PLATFORMS</h1>
+      		<hr id = "top_divider"></hr>
+      	</div>
+    </section>
     )
   }
 }
