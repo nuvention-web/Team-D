556e975006dfd1054e9f1d55deb0c2da8347e984
Megan Conlon <meganconlon2018@u.northwestern.edu>
2017-04-22 20:25:37 -0500

    test

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,17 +1,5 @@
 import React from 'react';
 
-<<<<<<< HEAD
-export const Title = (props) => {
-  return (
-    <section className="topTitle">
-      <div id = "top">
-      <img src={require("../../images/logo.png")} id = "logo_img"/>
-      <h1 id = "title"> > ALL PLATFORMS</h1>
-      <hr id = "top_divider"></hr>
-      </div>
-    </section>
-  );
-=======
 export class Title extends React.Component {
   constructor(props) {
     super(props);
@@ -22,5 +10,4 @@ export class Title extends React.Component {
       <p>This is Title</p>
     )
   }
->>>>>>> 642fd7aca318ee33a223bdb7fc1c444779904d0c
 }
