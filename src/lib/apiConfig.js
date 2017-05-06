1c702bafc41c9ff81bc9d7501a698a77a70672ea
Prithvi S S <prithvisrinivasasan-swachandam2017@u.northwestern.edu>
2017-05-06 16:09:36 -0500

    like,comments,reactions for all the videos

--- src/lib/apiConfig.js
+++ src/lib/apiConfig.js
@@ -1,13 +1,13 @@
-const ACCESS_TOKEN = "EAAR6flJrG9wBAOUYlIqDAGRs4yZBU6fTUQJZC8dxOkqvnjZADkLExaD7RhGtPRbncEtwPdMclrSPUayvq3p3HzYomVrvvmjGRqC2hg794zZAjevjMudFsscoFbmV1FcSq8O1ZALVDPSTQkgKBgUsKpvpIsA7491sZD";
+const ACCESS_TOKEN = "EAAKq7ZA42CkUBAJ6Ky0WWCCv1JMLawAmnLc7orlrdGIwYhaIOGlERK7L2lmgR44nOnYM2FaEN1eSj9cu0YphlTuTeYdaZA2ZCMzZCgoP7cZBnZBYiQEU2pQZAmK2Q5jILFg2lq6jE8ZBroUnbcVVBC4JW7MZCqjKop8ZBBBTB8kX6ECgZDZD";
 
 // Test Video # 1 ID
-const TEST1 = "10154546043703533";
+//const TEST1 = "10154546043703533";
 // Test Video #2 ID
-const TEST2 = "10154564632168533";
+const TEST2 = "1446916112038085";
 // Facebook Page Id
-const PAGE = "8947243532";
+const PAGE = "141553642574345";
 const params = {
-    fields: "video_insights{title,values},title,picture,source",
+    fields: "videos{likes,comments,reactions}",
     access_token: ACCESS_TOKEN
 }
 
@@ -15,13 +15,32 @@ export var facebookAPI = () => {
   return new Promise((resolve, reject) => {
     // Test Video # 1
     FB.api(
-        '/' + TEST2,
+        "/"  + PAGE,
         params,
         function (response) {
           if (response && !response.error) {
             /* handle the result */
-            // console.log("Test video #1 response: ", response);
+            console.log("Test video #1 response: ", response);
+            if (response.id) {
+              document.getElementById("title1").innerHTML = response.id;
+            } else {
+              document.getElementById("title1").innerHTML = "Facebook";
+            }
 
+            document.getElementById("image1").src = response.id;
+
+            /*var container1 = document.getElementById("content1");
+            var video_insights = response.video_insights.data;
+            for (var i = 0; i < video_insights.length; i++) {
+              let li = document.createElement('li');
+              let data = video_insights[i];
+              let title = data.title;
+              let value = data.values[0].value;
+              value = title + ": " + value;
+              li.innerHTML = value;
+              container1.appendChild(li);
+            }
+*/
           resolve(response);
 
           } else {
