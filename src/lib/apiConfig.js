0f0af539878c89eb7af57edd7e895fc49e874ae7
Prithvi S S <prithvisrinivasasan-swachandam2017@u.northwestern.edu>
2017-05-09 03:07:47 -0500

    organic paid total

--- src/lib/apiConfig.js
+++ src/lib/apiConfig.js
@@ -1,47 +1,88 @@
-const ACCESS_TOKEN = "EAACEdEose0cBACBgKyYS2VPoamdCcriVGDeH7QmKDGXhZA8Y8L8YpBdKAUzykZAEumHrO6CHi1sKjKKvyjRfUmJ2SSmWW0zk6KZBX0CfArokBqT6UhItvaW7txtIO8GTGgxPr3bfUSzngmGIyBeNLxY8ZAEZAcseO7HDIXJCP2IcIqziSzECYeE8oqdL0ZBiUZD";
+const ACCESS_TOKEN = "EAAaZArlfeNZAsBALbZBrpsLOpj8XkTiZAgKJcyQBF8ZBIYFJhE1crBRZCfdJuFkRrYnNt2W7DWFSYFxwUj79fGYzlORWAZAlXNLwBc6LmvKPrhZC9nejTeg53P6cIgnB7rub4wPS9OEzvK0kPw0eAf3baitTAChx7DuYqoUrE34LOwZDZD";
 
 // Test Video # 1 ID
 //const TEST1 = "10154546043703533";
 // Test Video #2 ID
 const TEST2 = "1446916112038085";
 // Facebook Page Id
-const PAGE = "14516439825";
-
+const PAGE = "141553642574345";
+/*  var today = "";
+  var month = d.getMonth() + 1;
+  var date = d.getDate();
 // const PAGE = "141553642574345";
-const params1 = {
-  since: "2017-01-01",
-  until:"2017-01-05",
-   access_token: ACCESS_TOKEN
-}
+var year = d.getFullYear()
+today += year + "-" + month + "-" + date;*/
+
 const params = {
-    fields: "videos{published,video_insights{values,period,title},likes,reactions,comments,title}",
     access_token: ACCESS_TOKEN
 }
 
 export var facebookAPI = () => {
 
 
-const pageInsights_promise = (res) =>{
-return new Promise((resolve, reject) => {
+const pageInsightsPaidPromise = (res) =>{
+
+  return new Promise((resolve, reject) => {
     // Test Video # 1
+
+
     FB.api(
-        "/"  + PAGE,
+        PAGE + "/insights/page_video_views_paid",
         params,
         function (response) {
           if (response && !response.error) {
+             console.log("Test video #1 response: ", response);
             /* handle the result */
             // console.log("Test video #1 response: ", response);
            // if(videos.paging.cursors.after != videos.paging.cursors.before){
            // console.log("Next returns ", response.videos.paging.next);
           //}
-           var request = $.ajax({
+/*           var request = $.ajax({
               type: "GET",
               url: response.videos.paging.next,
               dataType:"json"
             });
             request.done(function(msg){
               console.log("ajax response:" ,msg);
+            });*/
+            /*request.fail(function(jqXHR, textStatus) {
+              console.log( "Request failed: " + textStatus );
+              });*/
+
+             resolve(response);
+
+          } else {
+            console.error("error loading facebook video");
+            reject(response);
+          }
+        }
+      );
+    }
+)}
+const pageInsightsOrganicPromise = (res) =>{
+return new Promise((resolve, reject) => {
+    // Test Video # 1
+
+
+    FB.api(
+        PAGE + "/insights/page_video_views_organic",
+        params,
+        function (response) {
+          if (response && !response.error) {
+             console.log("Test video #2 response: ", response);
+            /* handle the result */
+            // console.log("Test video #1 response: ", response);
+           // if(videos.paging.cursors.after != videos.paging.cursors.before){
+           // console.log("Next returns ", response.videos.paging.next);
+          //}
+/*           var request = $.ajax({
+              type: "GET",
+              url: response.videos.paging.next,
+              dataType:"json"
             });
+            request.done(function(msg){
+              console.log("ajax response:" ,msg);
+            });*/
             /*request.fail(function(jqXHR, textStatus) {
               console.log( "Request failed: " + textStatus );
               });*/
@@ -60,13 +101,62 @@ return new Promise((resolve, reject) => {
     return new Promise((resolve, reject) =>{
     FB.api(
         PAGE + "/insights/page_video_views",
-        params1,
+        params,
         function (response) {
           if (response && !response.error) {
             /* handle the result */
             console.log("Test video #3 response: ", response);
-
-
+      /*      let day = {
+            current: {
+              views: 0,
+              interactions: 0
+            },
+            last: {
+              views: 0,
+              interactions: 0
+            }
+          }
+          const rows = response.rows;
+          const last = rows[0];
+          const current = rows[rows.length - 1];
+           day.last.views = last[1];
+           day.last.interactions = last[2] + last[3] + last[4];
+           day.current.views = current[1];
+           day.current.interactions = current[2] + current[3] + current[4];
+            let week = {
+            current: {
+              views: 0,
+              interactions: 0
+            },
+            last: {
+              views: 0,
+              interactions: 0
+            }
+          }
+          const rows = response.rows;
+          const last = rows[0];
+          const current = rows[rows.length - 1];
+          week.last.views = last[1];
+          week.last.interactions = last[2] + last[3] + last[4];
+          week.current.views = current[1];
+          week.current.interactions = current[2] + current[3] + current[4];
+           let month = {
+            current: {
+              views: 0,
+              interactions: 0
+            },
+            last: {
+              views: 0,
+              interactions: 0
+            }
+          }
+          const rows = response.rows;
+          const last = rows[0];
+          const current = rows[rows.length - 1];
+          month.last.views = last[1];
+          month.last.interactions = last[2] + last[3] + last[4];
+          month.current.views = current[1];
+          month.current.interactions = current[2] + current[3] + current[4];*/
 
 
             resolve(response);
@@ -81,7 +171,8 @@ return new Promise((resolve, reject) => {
   )
   }
     return new Promise((resolve, reject) => {
-      pageInsights_promise()
+      pageInsightsPaidPromise()
+        .then(pageInsightsOrganicPromise)
         .then(videointeractions_promise)
         .then((res) => {
           console.log("fb load complete");
