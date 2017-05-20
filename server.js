866fcf6ff13d4ec9cecb0950a50ebff7e1f27eef
lennyKim <heliotherapyy@gmail.com>
2017-05-19 23:49:26 -0500

    (add) brightcove in progress

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
 const path = require('path')
 const express = require('express')
+const Server = require('./server.js')
+const bodyParser = require('body-parser');
+const  brightcove = require('brightcove');
 
-module.exports = {
-  app: function () {
-    const app = express()
-    const indexPath = path.join(__dirname, './src/template.html')
-    // const publicPath = express.static(path.join(__dirname, './public'))
+const port = (process.env.PORT || 8080)
+const app = express()
+const indexPath = path.join(__dirname, './src/template.html')
 
-    // app.use('/public', publicPath)
-    app.use(express.static('public'))
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
+/* Middle Ware */
+app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-    return app
-  }
-}
+/* Routing */
+app.get('/', function (_, res) { res.sendFile(indexPath) });
+app.get('/api/brightcove', (req, res) => {
+  let a = "f5103281-757e-422e-8969-3744606da550";
+  let b = "D7W6AsYioOBZqiML9qjA";
+  let c = "1uVCSm0WysLQSey2409qiwZpsdtlEv4gzCvTgkNa";
+  let d = "Pm2pVKxeak92KD1wI9infdJA";
+
+  const oauthApi = new brightcove.OAuthApi(a, b + "-" + c + "-" + d);
+  oauthApi.getAccessToken((first, second) => {
+    const access_token = second["access_token"];
+    console.log("access token: ", access_token);
+    res.send(access_token);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(port)
+console.log(`Listening at http://localhost:${port}`)
---
+++ src/lib/brightcove.js
@@ -0,0 +1,11 @@
+console.log("brightcove");
+
+$.ajax({
+  url: "https://oauth.brightcove.com/v3/access_token?grant_type=client_credentials",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Authorization": "ZjUxMDMyODEtNzU3ZS00MjJlLTg5NjktMzc0NDYwNmRhNTUwOkQ3VzZBc1lpb09CWnFpTUw5cWpBLTF1VkNTbTBXeXNMUVNleTI0MDlxaXdacHNkdGxFdjRnekN2VGdrTmEtUG0ycFZLeGVhazkyS0Qxd0k5aW5mZEpB"
+  }
+}).then(res => {
+  console.log("res", res);
+}) ;
