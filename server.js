5ada00a17583606e9a6386ad8fd03f76aaee18e2
lennyKim <heliotherapyy@gmail.com>
2017-05-23 00:41:20 -0500

    (add) succeeds in brightcove calling. now i have to work on the metrics

--- server.js
+++ server.js
@@ -28,71 +28,69 @@ app.get('/api/brightcove', (req, res) => {
   let c = "1uVCSm0WysLQSey2409qiwZpsdtlEv4gzCvTgkNa";
   let d = "Pm2pVKxeak92KD1wI9infdJA";
 
-  const oauthApi = new brightcove.OAuthApi(a, b + "-" + c + "-" + d);
-  oauthApi.getAccessToken((first, second) => {
-    const access_token = second["access_token"];
-    const account_id = "1155968404";
-    const dimensions = ["video","device_type","device_os"];
-    const fields =["video_view"];
-    let from = {
-      daily: {
-        current: (new Date).getTime(),
-        last: "-2d"
-      },
-      weekly: {
-        current: "-7d",
-        last: "-14d"
-      },
-      monthly: {
-        current: "-28d",
-        last: "-56d"
+  const client_id = a;
+  const client_secret = b + "-" + c + "-" + d; // I do this because client_secret as it is creates an error somehow
+  const config = {
+      headers: {
+        'Authorization': 'Basic ' + new Buffer(client_id + ":" + client_secret).toString('base64'),
+        'Content-Type': 'application/x-www-form-urlencoded'
       }
-    };
-    let to = {
-      daily: {
-        current: (new Date).getTime(),
-        last: "-1d"
-      },
-      weekly: {
-        current: (new Date).getTime(),
-        last: "-7d"
-      },
-      monthly: {
-        current: (new Date).getTime(),
-        last: "-28d"
-      }
-    };
+    }
 
-    // axios.defaults.baseURL = 'https://api.example.com';
-    const config = {
-        headers: {
-          'Authorization': 'Bearer ' + access_token
-        }
-      }
+  axios.post("https://oauth.brightcove.com/v4/access_token?grant_type=client_credentials", {}, config)
+       .then(res => {
+          const access_token = res.data.access_token;
+          console.log("access token: ", access_token);
+          const account_id = "1155968404";
+          const dimensions = ["video","device_type","device_os"];
+          const fields =["video_view"];
+          let from = {
+            daily: {
+              current: (new Date).getTime(),
+              last: "-2d"
+            },
+            weekly: {
+              current: "-7d",
+              last: "-14d"
+            },
+            monthly: {
+              current: "-28d",
+              last: "-56d"
+            }
+          };
+          let to = {
+            daily: {
+              current: (new Date).getTime(),
+              last: "-1d"
+            },
+            weekly: {
+              current: (new Date).getTime(),
+              last: "-7d"
+            },
+            monthly: {
+              current: (new Date).getTime(),
+              last: "-28d"
+            }
+          };
 
-    // axios.get("https://analytics.api.brightcove.com/v1/data?accounts="
-    //           + account_id
-    //           + "&dimensions="
-    //           + dimensions.join(",")
-    //           + "&from=" + from.daily.last
-    //           + "&to=" + to.daily.last
-    //           + "&fields=" + fields.join(","))
-    //   .then(res => {
-    //     console.log("brightcove res:", res);
-    //   })
-    //   .catch(err => {
-    //     console.log("brightcove error:", err);
-    //   })
-    axios.get("https://analytics.api.brightcove.com/v1/data?accounts=1752604059001&dimensions=video&fields=video_impression,video_view,video.name", config)
-         .then(res => {
-            console.log("brightcove res:", res);
-         })
-         .catch(err => {
-            console.log("brightcove error:", err);
-         })
-
-    // res.status(200).send(access_token);
-  });
+          axios.get("https://analytics.api.brightcove.com/v1/data?accounts="
+                        + account_id
+                        + "&access_token=" + access_token
+                        + "&dimensions="
+                        + dimensions.join(",")
+                        + "&from=" + from.daily.last
+                        + "&to=" + to.daily.last
+                        + "&fields=" + fields.join(","))
+                .then(res => {
+                  console.log("brightcove res:", res);
+                })
+                .catch(err => {
+                  console.log("brightcove error:", err);
+                });
+       })
+       .catch(err => {
+        console.log("Brightcove OAuth error:", err);
+       });
 });
 
 
