c6c3bdabecf1a2456c92e38530343d497727a475
lennyKim <heliotherapyy@gmail.com>
2017-05-31 04:15:20 -0500

    (fix) change to radio buttons

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -10,18 +10,23 @@ export class CheckBox extends React.Component {
 
   render() {
     return (
-      <form>
+      <section>
         <h1 id="platforms" className="platforms-menu">Platforms</h1>
-          <h2>On-site</h2>
-            <label id = "ld_label"><input type="checkbox" id="cb" label={"On-Site"} checked={this.isOnSite}onChange={this.handleOnSiteChange}/>Brightcove</label>
-          <h2>Off-site</h2>
-          <label id = "ld_label"><input type="checkbox" id="cb" label={"Facebook"} checked={this.isFB} onChange={this.handleFBChange}/>Facebook</label><br/>
-          <label id = "ld_label"><input type="checkbox" id="cb" label={"Youtube"} checked={this.isYT} disabled={true} onChange={this.handleYTChange}/>Youtube</label><br/>
+          <form>
+            <h2>On-site</h2>
+              <label id = "ld_label"><input type="checkbox" id="cb" label={"On-Site"} checked={this.isOnSite}onChange={this.handleOnSiteChange}/>Brightcove</label>
+            <h2>Off-site</h2>
+            <label id = "ld_label"><input type="checkbox" id="cb" label={"Facebook"} checked={this.isFB} onChange={this.handleFBChange}/>Facebook</label><br/>
+            <label id = "ld_label"><input type="checkbox" id="cb" label={"Youtube"} checked={this.isYT} disabled={true} onChange={this.handleYTChange}/>Youtube</label><br/>
+          </form>
 
-        <h1 id="platforms" className="platforms-menu">Timeframe</h1>
-            <label id = "ld_label"><input type="checkbox" id="cb" label={"24 hours"} checked={this.isFB} onChange={this.handleFBChange}/>24 hours</label><br/>
-            <label id = "ld_label"><input type="checkbox" id="cb" label={"7 days"} checked={this.isYT} onChange={this.handleYTChange}/>7 days</label><br/>
-      </form>
+        <h1 id="timeframes" className="platforms-menu">Timeframe</h1>
+          <form>
+            <label id = "ld_label"><input type="radio" name="timeframe" id="cb" label={"24 hours"} checked={this.isFB} onChange={this.handleFBChange}/>24 hours</label><br/>
+            <label id = "ld_label"><input type="radio" name="timeframe" id="cb" label={"7 days"} checked={this.isYT} onChange={this.handleYTChange}/>7 days</label>
+          </form>
+      </section>
+
     )
   }
 }
