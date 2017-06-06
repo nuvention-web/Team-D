5ba375764c4de2f210e1848c013895163af95e1a
lennyKim <heliotherapyy@gmail.com>
2017-06-06 08:35:56 -0500

    please

--- src/app.jsx
+++ src/app.jsx
@@ -130,7 +130,7 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:8080/api/brightcove').then(res => {
+    axios.get('https://localhost:8080/api/brightcove').then(res => {
       console.log("brightcove: ", res);
       let data = {
         daily: {
